ec94d139f2dcaab278b6060d9f55c2d8d8e66056
Eugene <zohan1234567@gmail.com>
2019-07-27 22:49:41 +0300

    second commit Task#8

--- Task8/src/main/java/Task_8.java
+++ Task8/src/main/java/Task_8.java
@@ -1,8 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLPortType;
+
 import java.util.Scanner;
 
 public class Task_8 {
     public static void main(String[] args) {
-
         // 2. Найти max{min(a, b), min(c, d)}
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a");
@@ -13,8 +14,20 @@ public class Task_8 {
         int c = scanner.nextInt();
         System.out.println("Enter d");
         int d = scanner.nextInt();
-        int z = Math.max(Math.min(a,b),Math.min(c,d));
-        System.out.println("The value of max{min(a, b), min(c, d)} is " + z);
-        scanner.close();
+        int z = Math.max(Math.min(a, b), Math.min(c, d));
+        System.out.println("Test -> " + z);
+
+        int f , h ;
+        if (a >= b) {
+            f = b;
+        }else f = a;
+        if (c >= d) {
+            h = d;
+        }else h = c;
+        if (f >= h) {
+            System.out.println("The value of max{min(a, b), min(c, d)} is " + f);
+        }else System.out.println("The value of max{min(a, b), min(c, d)} is " + h);
+
+         scanner.close();
+        }
     }
-}
