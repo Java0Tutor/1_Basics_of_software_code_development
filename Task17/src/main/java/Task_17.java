057412cee33b6707ddc9d09b513cf4ab5246eaac
Eugene <zohan1234567@gmail.com>
2019-07-30 23:04:14 +0300

    first commit Task#17

--- Task17/src/main/java/Task_17.java
+++ Task17/src/main/java/Task_17.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Task_17 {
+    public static void main(String[] args) {
+
+         // Циклы
+        //6. Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
+
+        char charsSigns  = '\u0000';
+        int charsNumber = 0;
+        for (int i = 0; i<= 10000; i++){
+            charsSigns++;
+            charsNumber++;
+            System.out.println("sign: " + charsSigns + "  number: " + charsNumber);
+        }
+    }
 }
