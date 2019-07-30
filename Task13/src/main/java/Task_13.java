97ff722f39c1a2e8508880a2b11d2a2c1a8bc9a0
Eugene <zohan1234567@gmail.com>
2019-07-30 20:02:52 +0300

    first commit for Task_16

--- Task13/src/main/java/Task_13.java
+++ Task13/src/main/java/Task_13.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class Task_13 {
     public static void main(String[] args) {
 
-      //  2. Вычислить значения функции на отрезке [а,b] c шагом h:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter start line A");
+        int a = scanner.nextInt();
+        System.out.println("Enter finish line B");
+        int b = scanner.nextInt();
+        System.out.println("Enter step h");
+        double h = scanner.nextDouble();
+        System.out.println("Enter the value of x");
+        int x = scanner.nextInt();
+        int  y;
 
-
-
-        // Не понял условие
-
-
-
-
-
-
-
-
+        for (double i = a; i <= b; i = i + h) {
+            if (x > 2) {
+                y = x;
+            } else
+                y = -x;
+            System.out.println("x = "+ x + "\n" + "y = " + y);
+        }
     }
 }
