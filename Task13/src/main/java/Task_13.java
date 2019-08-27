ff108790dc0615e8f27e35d8edcae92d7e8e3f65
EugeneZakhno <30446748+EugeneZakhno@users.noreply.github.com>
2019-08-27 22:33:24 +0300

    Update Task_13.java

--- Task13/src/main/java/Task_13.java
+++ Task13/src/main/java/Task_13.java
@@ -1,48 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import java.util.Scanner;
-
-public class Task_13 {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter start line A");
-        int a = scanner.nextInt();
-        System.out.println("Enter finish line B");
-        int b = scanner.nextInt();
-        System.out.println("Enter step h");
-        double h = scanner.nextDouble();
-        System.out.println("Enter the value of x");
-        int x = scanner.nextInt();
-        int  y;
-
-        for (double i = a; i <= b; i = i + h) {
-            if (x > 2) {
-                y = x;
-            } else
-              y = -x;
-           System.out.println("x = "+ x + "\n" + "y = " + y);
-        }
-=======
-public class Task_13 {
-    public static void main(String[] args) {
 
       //  2. Вычислить значения функции на отрезке [а,b] c шагом h:
-
-
-
-        // Не понял условие
-
-
-
-
-
-
-
-
->>>>>>> Task_14
-=======
-import java.util.Scanner;
 
 public class Task_13 {
     public static void main(String[] args) {
@@ -65,6 +23,5 @@ public class Task_13 {
                 y = -x;
             System.out.println("x = "+ x + "\n" + "y = " + y);
         }
->>>>>>> Task_16
     }
 }
