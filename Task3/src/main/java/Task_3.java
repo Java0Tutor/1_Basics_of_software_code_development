6cb044b31a75288163864c212512033d32bf9433
EugeneZakhno <30446748+EugeneZakhno@users.noreply.github.com>
2019-08-27 22:23:03 +0300

    Update Task_3.java

--- Task3/src/main/java/Task_3.java
+++ Task3/src/main/java/Task_3.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Task_3 {
     public static void main(String[] args) {
-<<<<<<< HEAD
       //  3. Вычислить значение выражения по формуле (все переменные принимают действительные значения):
-
-
         public static void main(String[] args){
             Scanner scanner = new Scanner(System.in);
             double a, b, c;
@@ -15,27 +12,9 @@ public class Task_3 {
             b = scanner.nextDouble();
             System.out.println("Enter value of c");
             c = scanner.nextDouble();
-
             double z = ((a - 3) * b / 2) + c;
             System.out.println("The Result is = " + z);
             scanner.close();
         }
-
-
     }
 
-
-=======
-        Scanner scanner = new Scanner(System.in);
-        double x, y, c;
-        System.out.println("Enter value of x");
-        x = scanner.nextDouble();
-        System.out.println("Enter value of y");
-        y = scanner.nextDouble();
-        c = (Math.sin(x)+Math.cos(y))/(Math.cos(x) - Math.sin(y)) * Math.tan(x*y);
-        System.out.printf("The Result is = %.2f", c);
-        scanner.close();
-
-    }
->>>>>>> Task_3
-}
